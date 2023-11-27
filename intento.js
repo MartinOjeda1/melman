90a96f861f18245c4eccc49e01993a9d0d2b6ac7
“MartinOjeda1” <yomartinojeda@gmail.com>
2023-11-27 00:02:07 -0300

    se agrega el proyect

---
+++ intento.js
@@ -0,0 +1,7 @@
+let botonestrella = document.getElementById("botonestrella")
+let estrella = document.getElementById("estrella")
+
+botonestrella.onclick = function(){
+    estrella.src ="./imagenes/estrellas.jpg"
+}
+
